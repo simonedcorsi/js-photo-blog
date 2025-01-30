eeb0029bf0a19b607d819721af8d74bbfc04bed8
simonedcorsi <simonedomenico.corsi@gmail.com>
2025-01-30 12:42:52 +0100

    sistemazione

--- main.js
+++ main.js
@@ -10,8 +10,8 @@ fetch("https://lanciweb.github.io/demo/api/pictures/", { method: "GET" })
             const elements = `<div class= "bCard"> 
             <img id="pin" src="img/pin.svg" alt="pin">
                 <img id="img" src="${element.url}" alt="${element.title}">
-                <p>${element.title}</p>
                 <p class ="fontData">${element.date}</p>
+                <p>${element.title}</p>
             </div>`
 
             cards.innerHTML = elements;
