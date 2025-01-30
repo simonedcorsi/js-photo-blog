edd54399d042a71a27af064e4c59a9c32187a6be
simonedcorsi <simonedomenico.corsi@gmail.com>
2025-01-30 17:30:38 +0100

    prove

--- main.js
+++ main.js
@@ -1,41 +1,40 @@
 const card = document.getElementById('gallery');
 
 fetch("https://lanciweb.github.io/demo/api/pictures/", { method: "GET" })
-    .then(response => response.json())
-    .then(data => {
+.then(response => response.json())
+.then(data => {
 
-        let arrayCards = []
-        
-        data.forEach(element => {
-            const cards = document.createElement('div')
-            cards.className = "col-md-4 d-flex justify-content-center col-sm-6";
-            const elements = `<div class= "bCard" data-bs-toggle="modal" data-bs-target="#exampleModal"> 
-            <img id="pin" src="img/pin.svg" alt="pin">
-                <img id="img" src="${element.url}" alt="${element.title}">
-                <p class ="fontData">${element.date}</p>
-                <p>${element.title}</p>
-            </div>`
+    let arrayCards = []
+    
+    data.forEach(element => {
+        const cards = document.createElement('div')
+        cards.className = "col-md-4 d-flex justify-content-center col-sm-6";
+        const elements = `<div class= "bCard" data-bs-toggle="modal" data-bs-target="#exampleModal"> 
+        <img id="pin" src="img/pin.svg" alt="pin">
+            <img id="img" src="${element.url}" alt="${element.title}">
+            <p class ="fontData">${element.date}</p>
+            <p>${element.title}</p>
+        </div>`
 
-            cards.innerHTML = elements;
-            card.appendChild(cards);
+        cards.innerHTML = elements;
+        card.appendChild(cards);            
+    })
+    arrayCards = document.querySelectorAll('.bCard')
 
-            arrayCards = document.querySelectorAll('.bCard')
+    arrayCards.forEach(element => {
+        element.addEventListener('click', function () {
+            let modal = document.getElementById('modal');
+            modal.className = 'overlay'
 
-            arrayCards.forEach(element => {
-                element.addEventListener('click', function () {
-                    let modal = document.getElementById('modal');
-                    modal.className = 'overlay'
-                
-            })
-            document.getElementById("btn").addEventListener("click", function() {
-              
-                
-            } ) ;
-                
+            const pathImmagine = element.querySelector(img).png;
+            document.getElementById('preview_img').png = pathImmagine;
+            console.log(pathImmagine);
+
+            document.getElementById("btn").addEventListener("click", function () {
+                modal.className = 'overlay1'
+            });
         })
-
     });
-
 
 })
 
